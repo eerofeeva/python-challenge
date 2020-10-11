d523ed229ee47f41ef25ceb47d3d495d5255d0b2
Elena Erofeeva <eerofeeva8@gmail.com>
2020-10-10 20:43:15 -0500

    WIP: calculations done, not exportin yet

---
+++ PyBank/main.py
@@ -0,0 +1,51 @@
+# ## PyBank
+import os
+import csv
+import math 
+
+with open("Resources/budget_data.csv", 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    data = [ [row[0] , row[1]] for row in csvreader]
+
+#   * The total number of months included in the dataset
+total_months = len(data)-1
+
+#   * The net total amount of "Profit/Losses" over the entire period
+profit_loss_amounts_list = [int(x[1]) for x in data[1:]]
+months = [x[0] for x in data[1:]]
+
+total_profits = sum(profit_loss_amounts_list)
+
+#   * The average of the changes in "Profit/Losses" over the entire period
+
+change_by_month = [ y2-y1 for y1, y2  in zip(profit_loss_amounts_list[0:-1],profit_loss_amounts_list[1:])]
+
+average =float(sum(change_by_month))/(total_months-1)
+
+#   * The greatest increase in profits (date and amount) over the entire period
+max_profit = max(change_by_month)
+
+#   * The greatest decrease in losses (date and amount) over the entire period
+max_loss = min(change_by_month)
+
+for index, m in enumerate(change_by_month):
+    if m == max_profit:
+        max_index = index+1
+    if m == max_loss: 
+        min_index = index+1
+
+# * As an example, your analysis should look similar to the one below:
+print(total_months, total_profits, average,max_profit, max_loss)
+
+template='''
+   Financial Analysis
+   ----------------------------
+   Total Months: {}
+   Total: ${}
+   Average  Change: ${}
+   Greatest Increase in Profits: {} (${})
+   Greatest Decrease in Profits: {} (${})
+'''
+print(template.format(total_months, total_profits, average, months[max_index], max_profit, months[min_index], max_loss))
+# * In addition, your final script should both print the analysis to the terminal and export a text file with the results.
